d52c064b8ed72932a30dc3ed7f5411e529178ac1
Joni Roine <joni.roine@outlook.com>
2024-08-11 11:10:28 +0300

    added heronavbar for hero pages (still dont know how to make it automatic with props)

--- frontend-next-migration/src/app/[lng]/heroes/layout.tsx
+++ frontend-next-migration/src/app/[lng]/heroes/layout.tsx
@@ -1,18 +1,18 @@
-import {Navbar} from "@/widgets/Navbar";
+import { Navbar } from '@/widgets/Navbar';
 import { Footer } from '@/widgets/Footer';
-import {ReactNode} from "react";
+import { ReactNode } from 'react';
 
 interface Props {
-    children?: ReactNode;
+  children?: ReactNode;
+  navBarType?: string;
 }
 
-
-export default function Layout({ children}: Props) {
-    return (
-        <>
-            <Navbar overlaid={true} />
-            <main>{children}</main>
-            <Footer />
-        </>
-    )
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Navbar overlaid={true} navBarType='Heroes' />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
